dcd510ce2f5fc6b4893860b017726b5b6c05512a
tran49 <85269921+tran49@users.noreply.github.com>
2024-01-24 00:17:49 -0600

    Update serve.py

--- serve.py
+++ serve.py
@@ -4,21 +4,14 @@ import subprocess
 
 app = Flask(__name__)
 
-def stress_cpu():
-    # Run stress_cpu.py as a separate process
-    subprocess.Popen(["python", "stress_cpu.py"])
-
 @app.route('/', methods=['POST', 'GET'])
-def handle_requests():
+def main():
+    global seed
     if request.method == 'POST':
-        # Create a separate process for stressing CPU
-        stress_cpu()
-        return jsonify({"message": "Stressing CPU in a separate process"})
+        subprocess.run(["python3", "stress-cpu.py"])
+    else:
+        return socket.gethostname()
 
-    elif request.method == 'GET':
-        # Return private IP address of the EC2 instance
-        private_ip = socket.gethostbyname(socket.gethostname())
-        return jsonify({"private_ip": private_ip})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000, debug=True)
