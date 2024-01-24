39ac09fb28c9c0fb9c15ee10055aa6df3084da60
tran49 <85269921+tran49@users.noreply.github.com>
2024-01-24 02:39:32 -0600

    Update serve.py

--- serve.py
+++ serve.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def main():
     global seed
     if request.method == 'POST':
-        subprocess.Popen(["python", "stress_cpu.py"])
+        subprocess.Popen(["python3", "stress_cpu.py"])
         return jsonify({"message": "Stressing CPU in a separate process"})
 
     elif request.method == 'GET':
